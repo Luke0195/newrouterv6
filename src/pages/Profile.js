d5a3596f8916b2db30af5e848fcde7ae2600ac49
Lucas Santos <lucas201273@gmail.com>
2021-12-14 23:22:04 -0300

    move between screen using useNavigate hook

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 export function Profile(){
   const { id } = useParams()
+  const navigate = useNavigate()
+  const handleNavigate = () =>{
+    navigate('/')
+  }
   return(
-    <h1> Profiles {id}</h1>
+    <>
+      <h1> Profiles {id}</h1>
+      <button onClick={handleNavigate}> Move to Dashboard</button>
+    </>
   )
 }
